438908ea4fd2b4dfc6a4f4ae4be8e1b11e68f257
Seth Peters <thatguythatsbadwithnames@gmail.com>
2019-03-18 18:36:04 -0400

    Added forecast data for daily min and max temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+//add high and low daily value.
+
 const forecast = (latitude, longitude, callback) => {
 
     const url = 'https://api.darksky.net/forecast/61d4b51d04eda552406d0010ad3349f3/' +latitude +',' +longitude
@@ -10,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.')
         } else {
-            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. The high for today is ${body.daily.data[0].temperatureMax}. The low for today is ${body.daily.data[0].temperatureMin}.`)
         }
     })
 }
